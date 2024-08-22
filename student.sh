2ab05107ade512c3f3775867e0b9a40cd450e528
Ram <ramu.seerla@gmail.com>
2024-08-22 10:35:45 +0000

    commit1

---
+++ student.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#description: student info details
+#author: ram
+#date created:
+#version:
+#date modified:
+echo "student info details"
+read -p "please enter student name: "  student name
+read -p "please enter mobile number: " mobile number
+read -p "which course you looking for: " course name
+read -p "how you know about our institute: " referrel
+read -p "Looking for online or off line: " mode detailse
+echo "Thanks for visit NareshIT"
